f882a1d5fe4f7983b37f18485e7bbec7de63437c
dpbrian <brian.rojas@assistments.org>
2020-11-24 22:02:06 -0500

    Feature: Added my name and currenttime to title

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,18 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+		<ToDoList msg="Good Day Brian!"/>
+		<Time msg="The current time is "/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import ToDoList from './components/ToDoList.vue'
+import Time from './components/Time.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+	ToDoList,
+	Time,
   }
 }
 </script>
---
+++ src/components/Time.vue
@@ -0,0 +1,31 @@
+<template>
+	<div>
+
+		<h1>{{ msg }}{{ timestamp }}</h1>
+	</div>
+</template>
+
+<script>
+export default {
+	props: {
+		msg: String
+	},
+	data: function() { 
+		return {
+			timestamp: "",
+		} 
+	},
+	created() {
+			setInterval(this.getNow, 1000);
+		},
+	methods: {
+			getNow: function() {
+				const today = new Date();
+				const date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
+				const time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+				const dateTime = date +' '+ time;
+				this.timestamp = dateTime;
+			}
+		}
+}
+</script>
